ca39755187f02c11b8c73b850c3c1c182d3670df
TruyenVan <tvan@mymail.mines.edu>
2017-12-09 14:41:43 -0700

    Made the index, made the Login

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Home</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="author" content="Truyen Van">
+		<meta name="description" content="This website is for Assignment 0 of the Fall CSCI 445 class.">
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="csci445.css" />
+	</head>
+	<body>
+		</header>
+		<?php
+				$page = "home";
+				header("Location: ./login/login.php");
+				die();
+		?>
+	</body>
+</html>
