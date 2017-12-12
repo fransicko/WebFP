977122811385c9924f9d8cb7e0fa00153f8cf6d7
TruyenVan <tvan@mymail.mines.edu>
2017-12-12 16:39:11 -0700

    Updated header, login now logs into welcome page, templateContent since store pages might be the same later.

--- templateHeader.php
+++ templateHeader.php
@@ -1,10 +1,17 @@
-<link rel="stylesheet" type="text/css" href="header.css" />
 <header>
 	<nav>
-		<ul>
-			<li><a href="#">Children's</a></li>
-			<li><a href="#">Women's</a></li>
+		<ul id="navbar">
 			<li><a href="#">Men's</a></li>
+			<li><a href="#">Women's</a></li>
+			<li><a href="#">Children's</a></li>
+			<li class="dropdown">
+				<p class="dropbtn">Profile</p>
+				<div class="dropdowncontent">
+					<a href="#">Shopping Cart</a>
+					<a href="#">View Profile</a>
+					<a href="#">Log Out</a>
+				</div>
+			</li>
 		</ul>
 	</nav>
 </header>
