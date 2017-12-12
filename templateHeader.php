0325b2c07ada9a77c747a43f27cf6239997043ac
TruyenVan <tvan@mymail.mines.edu>
2017-12-12 15:44:03 -0700

    Added header.css, welcome page, and updated header.php

---
+++ templateHeader.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" type="text/css" href="header.css" />
+<header>
+	<nav>
+		<ul>
+			<li><a href="#">Children's</a></li>
+			<li><a href="#">Women's</a></li>
+			<li><a href="#">Men's</a></li>
+		</ul>
+	</nav>
+</header>
